366cd3778d035c9f7148ebed96a567ffc23ac81f
aneylon <arlen.m.neylon@gmail.com>
2026-03-17 20:13:21 -0600

    Add BorderLands Character component.

---
+++ src/Components/FiveAway/BorderLandsCharacter.js
@@ -0,0 +1,24 @@
+const BorderLandsCharacter = () => {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">Name :</label>
+        <label htmlFor="">Origin :</label>
+        <label htmlFor="">Agility :</label>
+        <label htmlFor="">Speed :</label>
+        <label htmlFor="">Combat Skill :</label>
+        <label htmlFor="">Tough :</label>
+        <label htmlFor="">Armor :</label>
+        <label htmlFor="">Luck :</label>
+        <label htmlFor="">Will :</label>
+        <label htmlFor="">Recovery :</label>
+        <label htmlFor="">Traits :</label>
+        <label htmlFor="">Equipment :</label>
+        <label htmlFor="">XP :</label>
+        <label htmlFor="">Advancements :</label>
+      </form>
+    </div>
+  );
+};
+
+export default BorderLandsCharacter;
