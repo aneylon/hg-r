2708ed4e200319c3045c528fc5aa5b621a59b5d4
aneylon <arlen.m.neylon@gmail.com>
2026-03-19 08:24:47 -0600

    Add editing mode to character sheet.

--- src/Components/FiveAway/BorderLandsCharacter.js
+++ src/Components/FiveAway/BorderLandsCharacter.js
@@ -1,10 +1,18 @@
-const BorderLandsCharacter = () => {
+import { useState } from "react";
+
+const BorderLandsCharacter = ({ newCharacter = false, character }) => {
+  const [editCharacter, setEditCharacter] = useState(false);
+  const clickEdit = () => {
+    setEditCharacter(!editCharacter);
+  };
   return (
     <div className="character">
+      <button onClick={clickEdit}>Edit</button>
       <form action="">
         <div>
           <label htmlFor="characterName">Name :</label>
           <input
+            disabled={!editCharacter}
             type="text"
             name="characterName"
             id="characterName"
@@ -13,22 +21,36 @@ const BorderLandsCharacter = () => {
         </div>
         <div>
           <label htmlFor="origin">Origin :</label>
-          <input type="text" name="origin" id="origin" placeholder="Origin" />
+          <input
+            disabled={!editCharacter}
+            type="text"
+            name="origin"
+            id="origin"
+            placeholder="Origin"
+          />
         </div>
         <div>
           <label htmlFor="agility">Agility :</label>
           <input
+            disabled={!editCharacter}
             type="text"
             name="agility"
             id="agility"
             placeholder="Agility"
           />
           <label htmlFor="speed">Speed :</label>
-          <input type="text" name="speed" id="speed" placeholder="Skill" />
+          <input
+            disabled={!editCharacter}
+            type="text"
+            name="speed"
+            id="speed"
+            placeholder="Skill"
+          />
         </div>
         <div>
           <label htmlFor="combatSkill">Combat Skill :</label>
           <input
+            disabled={!editCharacter}
             type="text"
             name="combatSkill"
             id="combatSkill"
@@ -37,17 +59,42 @@ const BorderLandsCharacter = () => {
         </div>
         <div>
           <label htmlFor="tough">Tough :</label>
-          <input type="text" name="tough" id="tough" placeholder="Tough" />
+          <input
+            disabled={!editCharacter}
+            type="text"
+            name="tough"
+            id="tough"
+            placeholder="Tough"
+          />
           <label htmlFor="armor">Armor :</label>
-          <input type="text" name="armor" id="armor" placeholder="Armor" />
+          <input
+            disabled={!editCharacter}
+            type="text"
+            name="armor"
+            id="armor"
+            placeholder="Armor"
+          />
         </div>
         <div>
           <label htmlFor="luck">Luck :</label>
-          <input type="text" name="luck" id="luck" placeholder="Luck" />
+          <input
+            disabled={!editCharacter}
+            type="text"
+            name="luck"
+            id="luck"
+            placeholder="Luck"
+          />
           <label htmlFor="will">Will :</label>
-          <input type="text" name="will" id="will" placeholder="Will" />
+          <input
+            disabled={!editCharacter}
+            type="text"
+            name="will"
+            id="will"
+            placeholder="Will"
+          />
           <label htmlFor="recovery">Recovery :</label>
           <input
+            disabled={!editCharacter}
             type="text"
             name="recovery"
             id="recovery"
@@ -56,11 +103,18 @@ const BorderLandsCharacter = () => {
         </div>
         <div>
           <label htmlFor="traits">Traits :</label>
-          <input type="text" name="traits" id="traits" placeholder="Traits" />
+          <input
+            disabled={!editCharacter}
+            type="text"
+            name="traits"
+            id="traits"
+            placeholder="Traits"
+          />
         </div>
         <div>
           <label htmlFor="equipment">Equipment :</label>
           <input
+            disabled={!editCharacter}
             type="text"
             name="equipment"
             id="equipment"
@@ -69,15 +123,28 @@ const BorderLandsCharacter = () => {
         </div>
         <div>
           <label htmlFor="xp">XP :</label>
-          <input type="text" name="xp" id="xp" placeholder="XP" />
+          <input
+            disabled={!editCharacter}
+            type="text"
+            name="xp"
+            id="xp"
+            placeholder="XP"
+          />
           <label htmlFor="advancements">Advancements :</label>
           <input
+            disabled={!editCharacter}
             type="text"
             name="advancements"
             id="advancements"
             placeholder="Advancements"
           />
         </div>
+        {newCharacter && (
+          <input disabled={!editCharacter} type="submit" value="Add" />
+        )}
+        {editCharacter && (
+          <input disabled={!editCharacter} type="submit" value="Update" />
+        )}
       </form>
     </div>
   );
