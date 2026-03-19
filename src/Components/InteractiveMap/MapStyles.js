fda0a9470cde9957ba796c4b16700263641527f5
aneylon <arlen.m.neylon@gmail.com>
2026-03-19 10:23:52 -0600

    Adjust tool tip styles.

--- src/Components/InteractiveMap/MapStyles.js
+++ src/Components/InteractiveMap/MapStyles.js
@@ -4,10 +4,6 @@ const MapStyles = () => {
   return (
     <div>
       <h1>Map Styles</h1>
-      <div className="toolTip">
-        yar?
-        <span className="toolTipText">This is some stuff</span>
-      </div>
 
       <div className="mapHolder">
         <div className="townCircle" style={{ top: 50, left: 50 }}></div>
@@ -17,8 +13,11 @@ const MapStyles = () => {
           style={{ top: 100, left: 185, width: 125, height: 125 }}
         ></div>
 
-        <div className="townCircle toolTip" style={{ bottom: 75, right: 100 }}>
-          <span className="toolTipText">Just a test</span>
+        <div className="townCircle" style={{ bottom: 75, right: 100 }}>
+          <div className="toolTip">
+            {" "}
+            <span className="toolTipText">Just a test</span>
+          </div>
         </div>
       </div>
     </div>
