b6ff671997ee3615cf8922f7d6e36f03a2fb7705
aneylon <arlen.m.neylon@gmail.com>
2026-05-07 12:33:51 -0600

    Create new game type select component.

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 import GameList from "../GameTypes/GameList";
+import GameTypeSelect from "../GameTypes/GameTypeSelect";
 
 const Game = () => {
   // todo load users existing games
   const [oldGames, setOldGames] = useState(null);
   const [newGame, setNewGame] = useState(false);
+  const [gameType, setGameType] = useState(null);
 
   const selectNewGame = () => {
     setNewGame(true);
+  };
+
+  const createNewGame = () => {
+    console.log("create new game", gameType);
   };
 
   useEffect(() => {
@@ -18,7 +24,12 @@ const Game = () => {
     <div>
       <h1>Select One</h1>
       <button onClick={selectNewGame}>New Game</button>
-      {newGame && <GameList />}
+      {newGame && (
+        <div>
+          <GameTypeSelect />
+          <button onClick={createNewGame}>Create New Game</button>
+        </div>
+      )}
       {oldGames && (
         <div>
           <select name="oldGames" id="oldGames">
---
+++ src/Components/GameTypes/GameTypeSelect.js
@@ -0,0 +1,13 @@
+const GameTypeSelect = () => {
+  return (
+    <div>
+      <h2>Game Type select</h2>
+      <select name="gameType" id="gameType">
+        <option value="Weird Fantasy">Weird Fantasy</option>
+        <option value="Space Opera">Space Opera</option>
+      </select>
+    </div>
+  );
+};
+
+export default GameTypeSelect;
