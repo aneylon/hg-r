e41b621770958597e3dc3f6fdf1276ba02182dfe
aneylon <arlen.m.neylon@gmail.com>
2026-05-08 19:25:58 -0600

    Update Game Type Select component to get options from db.

--- src/Components/GameTypes/GameTypeSelect.js
+++ src/Components/GameTypes/GameTypeSelect.js
@@ -1,11 +1,32 @@
+import { useEffect } from "react";
+import useFetch from "../../Hooks/useFetch";
+
 const GameTypeSelect = () => {
+  const { request, error, isLoading, data } = useFetch();
+
+  useEffect(() => {
+    console.log("get game types");
+    request("http://localhost:4200/gameTypes", "GET");
+  }, []);
   return (
     <div>
       <h2>Game Type select</h2>
-      <select name="gameType" id="gameType">
-        <option value="Weird Fantasy">Weird Fantasy</option>
-        <option value="Space Opera">Space Opera</option>
-      </select>
+      {isLoading && (
+        <div>
+          <h2>...Loading...</h2>
+        </div>
+      )}
+      {!isLoading && (
+        <select name="gameType" id="gameType">
+          {data.map((item) => {
+            return (
+              <option key={item.id} value={item.title}>
+                {item.title}
+              </option>
+            );
+          })}
+        </select>
+      )}
     </div>
   );
 };
