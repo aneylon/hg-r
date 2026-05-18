f07955881076a1ea499c428563fb3a5f070259fa
aneylon <arlen.m.neylon@gmail.com>
2026-05-18 12:08:19 -0600

    Setup game route and mock gametypes.

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ const morgan = require("morgan");
 const port = 4200;
 
 const user = require("./routes/user");
+const game = require("./routes/game");
 
 app.use(morgan("dev"));
 
 app.use("/user", user);
+app.use("/game", game);
 
 app.get("/", (req, res) => {
   res.send("Honor and Glory! (server)");
--- server/routes/game.js
+++ server/routes/game.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
+const gameTypes = [
+  {
+    id: "1",
+    title: "Mythic Britain",
+  },
+  {
+    id: "2",
+    title: "Weird Fantasy",
+  },
+  {
+    id: "3",
+    title: "Mythic Eastern Fantasy",
+  },
+  {
+    id: "4",
+    title: "Space Opera",
+  },
+];
+
 router.get("/", (req, res) => {
   res.send("get games");
+});
+
+router.get("/gameTypes", (req, res) => {
+  res.send(gameTypes);
 });
 
 module.exports = router;
