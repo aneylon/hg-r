f72321b469602fe1b165354ebc0c25176c7de676
aneylon <arlen.m.neylon@gmail.com>
2026-05-22 11:09:10 -0600

    Start routes for user sign in/up.

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,4 +5,12 @@ router.get("/", (req, res) => {
   res.send("get them users");
 });
 
+router.post("/signin", (req, res) => {
+  res.send("sign in existing user");
+});
+
+router.post("/signup", (req, res) => {
+  res.send("sign up new user");
+});
+
 module.exports = router;
