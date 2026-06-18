aeb76c8f63362b58840d443f04271cdfb351c377
aneylon <arlen.m.neylon@gmail.com>
2026-06-18 07:59:07 -0600

    Remove comments and finish mongo connections.

--- server/routes/example.js
+++ server/routes/example.js
@@ -2,44 +2,32 @@ const express = require("express");
 const Example = require("../models/Example");
 const router = express.Router();
 
-let examples = [
-  { id: 1, text: "one" },
-  { id: 2, text: "two" },
-  { id: 3, text: "three" },
-  { id: 4, text: "four" },
-];
+// TODO : Add Error catching around DB requests.
+// TODO : Create error catching middle ware?
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  // TODO : get from mongo
   const example = await Example.findById(id);
 
-  // res.send("Get comment for id : ", id);
   res.status(200).json(example);
 });
 
 router.get("/", async (req, res) => {
-  // TODO : get from mongo
   const examples = await Example.find();
 
-  // res.send(examples);
   res.status(200).json(examples);
 });
 
 router.post("/", async (req, res) => {
   let { newItem } = req.body;
-  console.log({ newItem });
   let example = await Example.create({ text: newItem });
-  // TODO : save to mongo
-  console.log("testrers", example);
-  res.send({ one: "1" });
+  res.status(200).json(example);
 });
 
-// TODO : Delete Route
-// Delete from mongo
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   let { id } = req.params;
-  res.send("delete id : " + id);
+  const deleted = await Example.deleteOne({ _id: id });
+  res.status(200).json(deleted);
 });
 
 module.exports = router;
