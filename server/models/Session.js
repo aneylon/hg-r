797f61894157739e04a8ff7f882d69e6c01a23c5
aneylon <arlen.m.neylon@gmail.com>
2026-06-24 10:01:53 -0600

    Add Session model.

---
+++ server/models/Session.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const sessionSchema = new Schema(
+  {
+    prompt: {
+      type: String,
+      required: true,
+    },
+    playerResponse: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Session", sessionSchema);
