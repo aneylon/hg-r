396a3235f0e3739528dc04823d7148d70eea0dde
aneylon <arlen.m.neylon@gmail.com>
2026-06-29 11:55:21 -0600

    Add Messages component.

---
+++ src/Components/Messages/Messages.js
@@ -0,0 +1,19 @@
+const Messages = () => {
+  // TODO : get message from server.
+
+  // Create New message component
+
+  return (
+    <div>
+      <h1>Messages</h1>
+      <div>
+        <h2>Message List</h2>
+        <ul>
+          <li>Message Example</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
