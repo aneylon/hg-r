509592021cfd90c445a998dbc110fe785dedaf79
aneylon <arlen.m.neylon@gmail.com>
2026-07-01 11:32:06 -0600

    Start to add new message area.

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -7,6 +7,17 @@ const Messages = () => {
     <div>
       <h1>Messages</h1>
       <div>
+        <h2>New Message</h2>
+        <select name="recipient" id="recipient">
+          <option value="userId">User Name</option>
+        </select>
+        <textarea
+          name="newMessage"
+          id="newMessage"
+          placeholder="New Message"
+        ></textarea>
+      </div>
+      <div>
         <h2>Message List</h2>
         <ul>
           <li>Message Example</li>
